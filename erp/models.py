36d7eb09c1b56ae7bfa9d7c4b5c8899d20f94cd8
Marcelo Buchalowicz <marcelo.buch@hotmail.com>
2024-08-02 21:52:21 -0300

    creating the 'products' Class and hiding system variables

--- erp/models.py
+++ erp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class products(models.Model):
+    Cod = models.CharField(max_length=244, null=False, blank=False)
+    Desc = models.CharField(max_length=244, null=False, blank=False)
+    Cod_Barra = models.CharField(max_length=299)
+    Status = models.IntegerField(null=False, blank=False)
+    Date_Create = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    Unit_Cost = models.FloatField(null=False, blank=False)
+    Sale_Price = models.FloatField(null=False, blank=False)
